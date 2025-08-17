91d026d037a400bce15daefa28f6f6a6ca6cd3c0
St8Razor <themist3@gmail.com>
2025-08-16 22:06:14 -0400

    Update SM_TD to v0.5.0

--- keymaps/hd_promethium/keymap.c
+++ keymaps/hd_promethium/keymap.c
@@ -103,7 +103,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [LAYER_NUM] = LAYOUT(
               _______,      KC_7,  KC_8,      KC_9,     KC_0,                     QK_DYNAMIC_TAPPING_TERM_UP,     _______,  _______,  _______,   _______,
-              _______,      KC_4,  KC_5,      KC_6,     KC_LEFT_ANGLE_BRACKET,    QK_DYNAMIC_TAPPING_TERM_PRINT,  _______,  _______,  _______,   _______,
+              _______,      KC_4,  KC_5,      KC_6,     KC_LEFT_ANGLE_BRACKET,    QK_DYNAMIC_TAPPING_TERM_PRINT,     KC_0,  _______,  _______,   _______,
     _______,  _______,      KC_1,  KC_2,      KC_3,     KC_RIGHT_ANGLE_BRACKET,   QK_DYNAMIC_TAPPING_TERM_DOWN,   _______,  _______,  _______,   _______, _______,
 
                                    _______,   _______,  _______,                  _______,  _______,  _______
